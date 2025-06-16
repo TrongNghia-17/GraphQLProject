05e8bde8e06acd51fe37cd2ecc2e89b42f45310d
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-06-16 14:40:36 +0700

    22

---
+++ GraphQLProject/Program.cs
@@ -0,0 +1,22 @@
+using GraphQLProject.Interfaces;
+using GraphQLProject.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddTransient<IMenuRepository, MenuRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ GraphQLProject/Services/MenuRepository.cs
@@ -0,0 +1,43 @@
+﻿using GraphQLProject.Interfaces;
+using GraphQLProject.Models;
+
+namespace GraphQLProject.Services;
+
+public class MenuRepository : IMenuRepository
+{
+    private static List<Menu> MenuList = new List<Menu>()
+    {
+        new Menu(){Id = 1, Name = "One", Description="1", Price=100},
+        new Menu(){Id = 2, Name = "Two", Description="2", Price=200},
+        new Menu(){Id = 3, Name = "Three", Description="3", Price=300},
+        new Menu(){Id = 4, Name = "Four", Description="4", Price=400},
+        new Menu(){Id = 5, Name = "Five", Description="5", Price=500},
+    };
+
+    public Menu AddMenu(Menu menu)
+    {
+        MenuList.Add(menu);
+        return menu;
+    }
+
+    public void DeleteMenu(int id)
+    {
+        MenuList.RemoveAt(id);
+    }
+
+    public List<Menu> GetAllMenu()
+    {
+        return MenuList;
+    }
+
+    public Menu GetMenuById(int id)
+    {
+        return MenuList.Find(m => m.Id == id);
+    }
+
+    public Menu UpdateMenu(int id, Menu menu)
+    {
+        MenuList[id] = menu;
+        return menu;
+    }
+}
